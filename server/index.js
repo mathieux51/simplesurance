f110663551a6f6601cff9a7705095b30fda3d669
Mathieu <mathieux51@gmail.com>
2018-07-11 20:54:08 +0200

    Update port (Heroku)

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const log = bunyan.createLogger({
   stream: bunyanFormat({ outputMode: 'short' })
 })
 const host = 'localhost'
-const port = 3000
+const port = process.env.PORT || 3000
 
 const app = express()
 
